9441f06cb8e6ed30fb5eaa44c038e726b6d1c322
Takuma <i.taku0415@gmail.com>
2020-09-22 15:34:57 +0900

    [add] commentout

--- lib/views/main_screen.dart
+++ lib/views/main_screen.dart
@@ -22,48 +22,42 @@ class MainScreen extends StatelessWidget {
     )
   ];
 
+  // 下タブのタップによって表示される画面を配列に入れる
   final List<StatelessWidget> _screens = [
     FirstScreen(),
     SecondScreen(),
     ThirdScreen(),
   ];
 
-  final List<String> _titles = [
-    'あなたの秘密',
-    'みんなの秘密',
-    '設定'
-  ];
+  // タイトルも下タブによって変わるので配列に入れる
+  final List<String> _titles = ['あなたの秘密', 'みんなの秘密', '設定'];
 
   @override
   Widget build(BuildContext context) {
     return ChangeNotifierProvider<MainScreenController>(
-      create: (_) => MainScreenController(),
-      builder: (context, _) {
-        return Scaffold(
-          appBar: AppBar(
-            title: Consumer<MainScreenController>(
-              builder: (_, model, __) {
-                return Text(_titles[model.selectedTabIndex]);
-              }
-            )
-          ),
-          body: Consumer<MainScreenController>(
-            builder: (_, model, __) {
+        create: (_) => MainScreenController(),
+        builder: (context, _) {
+          return Scaffold(
+            appBar: AppBar(
+                title: Consumer<MainScreenController>(builder: (_, model, __) {
+              // selectedTabIndexの値によってタイトルが変わる
+              return Text(_titles[model.selectedTabIndex]);
+            })),
+            body: Consumer<MainScreenController>(builder: (_, model, __) {
+              // selectedTabIndexの値によって表示される画面が変わる
               return _screens[model.selectedTabIndex];
-            }
-          ),
-          bottomNavigationBar: Consumer<MainScreenController>(
-            builder: (_, model, __) {
+            }),
+            bottomNavigationBar:
+                Consumer<MainScreenController>(builder: (_, model, __) {
               return BottomNavigationBar(
                 items: _bottomNavItems,
                 currentIndex: model.selectedTabIndex,
                 selectedItemColor: Colors.amber[800],
+                // selectedTabIndexの値を変更する関数
                 onTap: model.changeTabIndex,
               );
-            }
-          ),
-        );
-      }
-    );
+            }),
+          );
+        });
   }
 }
